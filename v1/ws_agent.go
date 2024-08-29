934c08de5f7208fc5b79d8292ac4e9f812c71268
eric.li <lee64690502@gmail.com>
2024-08-29 14:07:00 +0800

    feat: modify websocket Agent
    - 原先使用注入field的方式去新增function 會造成在使用 interface 的時候被判定為沒有implement functions

--- v1/ws_agent.go
+++ v1/ws_agent.go
@@ -14,23 +14,32 @@ type IWsClient interface {
 
 type webSocketAgent[T IWsClient] struct {
 	Client T
-	Subscribe func(string)
-	Send func([]byte) error
-	Connect func(string) (*http.Response, error)
-	StartLoop func()
-	Stop func() error
 }
 
 func NewWebSocketAgent[T IWsClient](client T) *webSocketAgent[T] {
-	agent := &webSocketAgent[T]{
+	return &webSocketAgent[T]{
 		Client: client,
-		Subscribe: WithSubscribe(client),
-		Send: client.Send,
-		Connect: client.Connect,
-		StartLoop: client.StartLoop,
-		Stop: client.Stop,
 	}
-	return agent
+}
+
+func (a *webSocketAgent[T]) Subscribe(params string) {
+	a.Client.Subscribe(params)
+}
+
+func (a *webSocketAgent[T]) Send(request []byte) error {
+	return a.Client.Send(request)
+}
+
+func (a *webSocketAgent[T]) Connect(url string) (*http.Response, error) {
+	return a.Client.Connect(url)
+}
+
+func (a *webSocketAgent[T]) Stop() error {
+	return a.Client.Stop()
+}
+
+func (a *webSocketAgent[T]) StartLoop() {
+	a.Client.StartLoop()
 }
 
 func (a *webSocketAgent[T]) SendString(msg string) {
