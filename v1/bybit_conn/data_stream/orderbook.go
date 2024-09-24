a20cdb1d686239ca7b8f84396b6cbbb7f60dffc9
eric.li <lee64690502@gmail.com>
2024-09-24 13:09:16 +0800

    fix: v1 orderbook miss topic and symbol

--- v1/bybit_conn/data_stream/orderbook.go
+++ v1/bybit_conn/data_stream/orderbook.go
@@ -46,6 +46,8 @@ func (ob *OrderBook) Update(rawdata []byte) (*exchange_conn.OrderBookStream, err
 	default:
 		return nil, errors.New("unknown type")
 	}
+	ret.Topic = string(raw.GetStringBytes("topic"))
+	ret.Symbol = string(raw.GetStringBytes("data", "s"))
 	ret.Bids = BestMap(ob.Bids, -ob.BestDepth)
 	ret.Asks = BestMap(ob.Asks, ob.BestDepth)
 	return ret, nil
